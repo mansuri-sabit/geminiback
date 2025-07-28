3baf9e373229a0efaa94694bb2077defe96ca48d
mansuri-sabit <blueflamesabitali2003@gmail.com>
2025-07-28 19:08:53 +0530

    chnage backend url all

--- static/js/jevi-chat-widget.js
+++ static/js/jevi-chat-widget.js
@@ -3,7 +3,7 @@
     class JeviChatWidget {
         constructor(config) {
             this.projectId = config.projectId;
-            this.apiUrl = config.apiUrl || 'https://troikabackend.onrender.com';
+            this.apiUrl = config.apiUrl || 'https://geminiback-nxqj.onrender.com/';
             this.position = config.position || 'bottom-right';
             this.theme = config.theme || 'light';
             this.width = config.width || '400px';
